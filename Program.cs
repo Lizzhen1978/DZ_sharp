883c454e084b9d97700d18418829130b121a2436
Elizaveta Novikova <Lizzhen1978@yandex.ru>
2022-08-07 20:11:13 +0300

    Первая задача

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+int n1 = System.Int32.Parse(Console.ReadLine());
+int n2 = System.Int32.Parse(Console.ReadLine());
+
+if (n1 > n2) {
+    Console.WriteLine("Первое больше");
+} else {
+    Console.WriteLine("Второе больше");
+}
