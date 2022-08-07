168b1474aa6fc03a1205b1187f307a964d97a2ba
Elizaveta Novikova <Lizzhen1978@yandex.ru>
2022-08-07 20:13:57 +0300

    Вторая задача

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿// Первая задача
 int n1 = System.Int32.Parse(Console.ReadLine());
 int n2 = System.Int32.Parse(Console.ReadLine());
 
@@ -7,3 +7,17 @@ if (n1 > n2) {
 } else {
     Console.WriteLine("Второе больше");
 }
+
+
+// Вторая задача
+
+int n3 = System.Int32.Parse(Console.ReadLine());
+int n4 = System.Int32.Parse(Console.ReadLine());
+int n5 = System.Int32.Parse(Console.ReadLine());
+
+int max = Math.Max(Math.Max(n3, n4), n5);
+
+Console.WriteLine(max);
+
+// Третья задача
+
